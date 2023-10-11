c757cc40759da56ab428a8bfbd0a368525835560
Goraniya Karan <karangoraniya@gmail.com>
2023-10-11 23:17:09 +0530

    feat: add did ud resolver 🚀

--- app/page.tsx
+++ app/page.tsx
@@ -1,113 +1,101 @@
-import Image from 'next/image'
+'use client';
+import { useRef, useState } from 'react';
+import Resolution from '@unstoppabledomains/resolution';
 
-export default function Home() {
+const Home = () => {
+  const [domain, setDomain] = useState<string>('did:ud:');
+  const [resolvedAddress, setResolvedAddress] = useState<string | null>(null);
+
+  const [copySuccess, setCopySuccess] = useState<string>('');
+
+  const textAreaRef = useRef<HTMLTextAreaElement | null>(null);
+
+  const ethereumProviderUrl = 'https://rpc.ankr.com/eth';
+  const polygonProviderUrl = 'https://rpc.ankr.com/polygon';
+
+  const resolution = new Resolution({
+    sourceConfig: {
+      uns: {
+        locations: {
+          Layer1: {
+            url: ethereumProviderUrl,
+            network: 'mainnet',
+          },
+          Layer2: {
+            url: polygonProviderUrl,
+            network: 'polygon-mainnet',
+          },
+        },
+      },
+    },
+  });
+
+  const resolveDomain = async () => {
+    try {
+      const currency = 'ETH';
+      const address = await resolution.addr(
+        domain.replace('did:ud:', '').trim(),
+        currency
+      );
+      if (!address) {
+        return;
+      }
+      setResolvedAddress(address);
+    } catch (error) {
+      console.error('Error resolving domain:', error);
+    }
+  };
+
+  const copyToClipboard = (e: React.MouseEvent) => {
+    if (textAreaRef.current) {
+      textAreaRef.current.select();
+      document.execCommand('copy');
+      setCopySuccess('Copied!');
+    }
+  };
+
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <div className="z-10 max-w-5xl w-full items-center justify-between font-mono text-sm lg:flex">
-        <p className="fixed left-0 top-0 flex w-full justify-center border-b border-gray-300 bg-gradient-to-b from-zinc-200 pb-6 pt-8 backdrop-blur-2xl dark:border-neutral-800 dark:bg-zinc-800/30 dark:from-inherit lg:static lg:w-auto  lg:rounded-xl lg:border lg:bg-gray-200 lg:p-4 lg:dark:bg-zinc-800/30">
-          Get started by editing&nbsp;
-          <code className="font-mono font-bold">app/page.tsx</code>
-        </p>
-        <div className="fixed bottom-0 left-0 flex h-48 w-full items-end justify-center bg-gradient-to-t from-white via-white dark:from-black dark:via-black lg:static lg:h-auto lg:w-auto lg:bg-none">
-          <a
-            className="pointer-events-none flex place-items-center gap-2 p-8 lg:pointer-events-auto lg:p-0"
-            href="https://vercel.com?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            By{' '}
-            <Image
-              src="/vercel.svg"
-              alt="Vercel Logo"
-              className="dark:invert"
-              width={100}
-              height={24}
-              priority
-            />
-          </a>
+    <div className="flex flex-col items-center justify-center min-h-screen bg-black text-white">
+      <h1 className="text-2xl mb-4 font-bold">Domain Resolver</h1>
+
+      <div className="w-1/3 p-6 bg-gray-800 rounded-lg shadow-md">
+        <div className="flex items-center mb-4 border rounded">
+          <div className="p-3 bg-gray-700 text-white">did:ud:</div>
+          <input
+            className="flex-grow p-3 rounded-r text-black bg-white focus:outline-none"
+            value={domain}
+            onChange={(e) => setDomain(e.target.value)}
+            placeholder="Enter the rest of the domain"
+          />
         </div>
+        <button
+          className="w-full p-3 mt-4 border-0 rounded bg-blue-600 text-white hover:bg-blue-700 transition duration-200"
+          onClick={resolveDomain}
+        >
+          Resolve
+        </button>
+        {resolvedAddress && (
+          <div className="mt-4 flex items-center justify-between">
+            <textarea
+              ref={textAreaRef}
+              readOnly
+              value={resolvedAddress}
+              className="w-3/4 border p-3 rounded bg-white text-black"
+            />
+            <button
+              onClick={copyToClipboard}
+              className="ml-4 p-3 bg-green-600 rounded hover:bg-green-700 transition duration-200"
+            >
+              Copy
+            </button>
+          </div>
+        )}
+        {copySuccess && (
+          <div className="mt-4 text-center text-green-500">{copySuccess}</div>
+        )}
       </div>
+    </div>
+  );
+};
 
-      <div className="relative flex place-items-center before:absolute before:h-[300px] before:w-[480px] before:-translate-x-1/2 before:rounded-full before:bg-gradient-radial before:from-white before:to-transparent before:blur-2xl before:content-[''] after:absolute after:-z-20 after:h-[180px] after:w-[240px] after:translate-x-1/3 after:bg-gradient-conic after:from-sky-200 after:via-blue-200 after:blur-2xl after:content-[''] before:dark:bg-gradient-to-br before:dark:from-transparent before:dark:to-blue-700 before:dark:opacity-10 after:dark:from-sky-900 after:dark:via-[#0141ff] after:dark:opacity-40 before:lg:h-[360px] z-[-1]">
-        <Image
-          className="relative dark:drop-shadow-[0_0_0.3rem_#ffffff70] dark:invert"
-          src="/next.svg"
-          alt="Next.js Logo"
-          width={180}
-          height={37}
-          priority
-        />
-      </div>
-
-      <div className="mb-32 grid text-center lg:max-w-5xl lg:w-full lg:mb-0 lg:grid-cols-4 lg:text-left">
-        <a
-          href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Docs{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Find in-depth information about Next.js features and API.
-          </p>
-        </a>
-
-        <a
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Learn{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Learn about Next.js in an interactive course with&nbsp;quizzes!
-          </p>
-        </a>
-
-        <a
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Templates{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Explore the Next.js 13 playground.
-          </p>
-        </a>
-
-        <a
-          href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Deploy{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Instantly deploy your Next.js site to a shareable URL with Vercel.
-          </p>
-        </a>
-      </div>
-    </main>
-  )
-}
+export default Home;
